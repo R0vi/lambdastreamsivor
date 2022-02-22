7b1b7b8ebaa3a1e87ece77415e833f88ae657e31
Ivor Staats <Ivor.Staats@infosupport.com>
2022-02-22 10:37:09 +0100

    add demo2

--- demo02-execute-around/src/test/java/com/infosupport/demo02executearound/ExecuteAroundTests.java
+++ demo02-execute-around/src/test/java/com/infosupport/demo02executearound/ExecuteAroundTests.java
@@ -1,11 +1,13 @@
 package com.infosupport.demo02executearound;
 
+import org.jooq.lambda.Unchecked;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,19 +15,37 @@ public class ExecuteAroundTests {
     
     @Test
     @DisplayName("""
-    Execute Around Pattern
+    Execute Around Pattern print one line from file.
     """)
-    void executeAroundDemo(){
-        //Arrange
+    void executeAroundDemoPrintOneLine(){
+        readFile(Unchecked.consumer(bfr -> System.out.println(bfr.readLine())));
+    }
+
+    @Test
+    @DisplayName("""
+    Execute Around Pattern count words in file
+    """)
+    void executeAroundDemoCountWordsInFile(){
+        readFile(Unchecked.consumer(bfr -> {
+            int wordCount = 0;
+            while (bfr.ready()){
+                wordCount += bfr.readLine().split("\\s+").length;
+            }
+            System.out.println(wordCount);
+        }));
+    }
+
+    void readFile(Consumer<BufferedReader> consumer) {
         try(
-            final var fileReader = new FileReader("maxverstappen.txt");
-            final var bufferedReader = new BufferedReader(fileReader);){
+                final var fileReader = new FileReader("maxverstappen.txt");
+                final var bufferedReader = new BufferedReader(fileReader);){
+
+            consumer.accept(bufferedReader);
 
 
         }catch (Exception e){
             //log
             //throw exception after enriching it with context
         }
-
     }
 }
